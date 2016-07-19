9245707bfa2a1722114f8425de434b5e1f2d04ad
Roman Rytov <rytoff@stels.net>
2016-07-19 16:31:46 +0300

    last upload

---
+++ js/jscript.js
@@ -0,0 +1,32 @@
+﻿/*$(document).ready(function()	{
+	$('#gang').click(function()	{
+		
+		$('.team').css({"display": "none"}, "slow");
+	});
+	$('#homeW').click(function()	{
+		$('.team').css({"display": "none"}, "slow");
+		$('.main').css({"display": "none"}, "slow");
+	});
+	$('#group').click(function()	{
+		$('.team').css({"display": "block"}, "fast");
+});
+});*/
+
+$(document).ready(function() {
+	var stime = 0;
+	$("a.anchorLink").click(function () {
+
+		elementClick = $(this).attr("href");
+
+		destination = $(elementClick).offset().top;
+
+		$("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination}, 1100 );
+
+		clearTimeout(stime);
+		stime = setTimeout(function(){
+			window.location.hash = elementClick;
+		}, 1000);
+		
+		return false;
+	})
+});
